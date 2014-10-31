83f936707993cf9e9cdd80d4d1b98790aebb92d3
nadim <i.nadim@gmail.com>
2014-10-31 13:33:52 -0400

    Setting port for heroku, deleting some old files

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ app.get('/', function(req,res) {
     res.render('homepage');
 });
 
-var server = app.listen(3000, function () {
-	console.log('listening on port 3000');
+app.listen(process.env.PORT || 3000, function() {
+  console.log("listening on 3000");
 });
+
 
