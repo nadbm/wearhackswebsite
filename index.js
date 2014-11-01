39de2ff5bf8c499798ccfb5438653a45d65431af
ueg1990 <uehtesham90@gmail.com>
2014-11-01 14:45:18 -0400

    Add post action for mailchimp

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+chimp = require('./chimp')
 
 //use ejs templating
 app.set('view engine','ejs');
@@ -7,6 +8,8 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req,res) {
     res.render('homepage');
 });
+
+app.post('/s', chimp.subscribe);
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("listening on 3000");
